f5be1ea21fd8df49b5cabac437db2cd3c57ee4f4
Jack Tomlinson <tomlinson.johnq@gmail.com>
2026-02-18 21:37:20 -0600

    spotipy integration to fix authentication

--- test.py
+++ test.py
@@ -1,27 +1,29 @@
 import os
 from dotenv import load_dotenv
-import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-#ENV vars
-token = os.getenv('SPOTIFY_TOKEN')
+# Using Spotipy to handle OAuth flow automatically
+# This will open a browser to authenticate the user
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIFY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
+    redirect_uri="http://127.0.0.1:8888/callback",
+    scope="user-top-read"
+))
 
-#overall function to fetch data from spotify web api
-def fetch_web_api(endpoint, method, body):
-    res = requests.request(method, f'https://api.spotify.com/{endpoint}', headers={
-        'Authorization': f'Bearer {token}',
-    }, json=body)
-    return res.json()
-
-
-def get_top_tracks():
-    return fetch_web_api('v1/me/top/tracks?time_range=long_term&limit=5', 'GET', None)['items']
-
-
-top_tracks = get_top_tracks()
-print(len(top_tracks))
-for track in top_tracks:
-    name = track['name']
-    artists = ', '.join(artist['name'] for artist in track['artists'])
-    print(f'{name} by {artists}')
+# Call the API using Spotipy methods
+results = sp.currently_playing()
+print(results)
+if results and 'items' in results:
+    top_tracks = results['items']
+    print(top_tracks)
+    print(len(top_tracks))
+    for track in top_tracks:
+        name = track['name']
+        artists = ', '.join(artist['name'] for artist in track['artists'])
+        print(f'{name} by {artists}')
+else:
+    print("No top tracks found or error occurred.")
